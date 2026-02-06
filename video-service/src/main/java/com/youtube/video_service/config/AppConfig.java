8daa9f8abecaec005cfcfa47f39b74e37a2423b9
chirag15898-lang <chirag15898@gmail.com>
2026-02-06 11:58:54 +0530

    Video-Service added

---
+++ video-service/src/main/java/com/youtube/video_service/config/AppConfig.java
@@ -0,0 +1,47 @@
+package com.youtube.video_service.config;
+
+import com.youtube.video_service.util.ApiTemplate;
+import com.youtube.video_service.util.ApiTemplateImpl;
+import io.imagekit.sdk.ImageKit;
+import io.imagekit.sdk.config.Configuration;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Properties;
+
+@org.springframework.context.annotation.Configuration
+public class AppConfig {
+
+    @Value("${image.url.endpoint}") //@Value annotation help us to get the value of a property defied ins app properties file
+    String urlEndPoint;
+    @Value("${image.private.key}")
+    String privateKey;
+    @Value("${image.public.key}")
+    String publicKey;
+
+    @Bean
+    public ImageKit getImageKit() throws Exception{
+        ImageKit imageKit = ImageKit.getInstance();
+        Configuration configuration = new Configuration(publicKey, privateKey, urlEndPoint);
+        imageKit.setConfig(configuration);
+        return imageKit;
+    }
+
+    @Bean
+    public ModelMapper getMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public ApiTemplate getApiTemplate(){
+        return new ApiTemplateImpl();
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
