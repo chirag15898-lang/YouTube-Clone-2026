79e4c4542cf9626a61e6a5c154deb3841280e7f5
chirag15898-lang <chirag15898@gmail.com>
2026-02-07 18:36:21 +0530

    Tag service

---
+++ central/src/main/java/com/youtube/central/repository/TagRepo.java
@@ -0,0 +1,13 @@
+package com.youtube.central.repository;
+
+import com.youtube.central.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepo extends JpaRepository<Tag, UUID> {
+
+    public Tag findByName(String tagName);
+}
